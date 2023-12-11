cad1eb42b4afc5825362570d8863e843185e85cb
UserImranMik <imrankhan.m@infocareerindia.com>
2023-12-11 15:45:56 +0530

    retry success four

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,15 +1,17 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/sample")
 public class DemoController {
 	
 	@GetMapping("/demo")
 	public String demodocker() {
 		
-		return "Demo-Docker";		
+		return "HELLO WORLD";		
 	}
 
 }
